70615ccd94b8d7abf0c5b50c74b2c0b7311c06c0
Saksham-Jain-2007 <sakshamjain7017@gmail.com>
2025-09-17 16:09:55 +0530

    Add files via upload

---
+++ Day22/Day 22 Q44.c
@@ -0,0 +1,20 @@
+// Q44: Write a program to calculate the approximate value of the series 1 + 3/4 + 5/6 + ... up to n terms.
+ 
+ // PROGRAM:
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    float sum = 0.0;
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {
+        sum += (float)(2 * i - 1) / (i + 1);
+    }
+
+    printf("Approximate sum: %.1f\n", sum);
+
+    return 0;
+}
